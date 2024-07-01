581d664733f1d89458ea78fd739c2a9ad6e0af2d
Wanfredo Vargas <wanfrevargas@gmail.com>
2024-07-01 18:29:09 -0400

    Agregados mas estilos

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'shared-sidebar',
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
 
